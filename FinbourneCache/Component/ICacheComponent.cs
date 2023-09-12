a8bd6a1850e5aa1de3707afca58267c8a3044a9c
andreluizleite <andreluiz_leite@hotmail.com>
2023-09-12 12:28:24 +0100

    Added Raise Event and Errors Handle

--- FinbourneCache/Component/ICacheComponent.cs
+++ FinbourneCache/Component/ICacheComponent.cs
@@ -2,6 +2,8 @@
 {
     public interface ICacheComponent<TKey, TValue>
     {
+        event Action<TKey, TValue> _ItemEvicted;
+
         void Add(TKey key, TValue value);
         bool TryGet(TKey key, out TValue value);
     }
